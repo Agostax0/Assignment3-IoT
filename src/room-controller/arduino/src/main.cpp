f13b7a1d0b7b23068071b2763c3228b50b268915
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2023-03-12 15:11:06 +0100

    implemented queue

--- src/room-controller/arduino/src/main.cpp
+++ src/room-controller/arduino/src/main.cpp
@@ -1,26 +1,31 @@
-#include"SoftwareSerial.h"
+#include "SoftwareSerial.h"
 #include <Arduino.h>
 #include <ServoTimer2.h>
 #include <MyMsg.h>
-SoftwareSerial bluetooth_serial(8,9);
+#include <EventQueue.h>
+SoftwareSerial bluetooth_serial(8, 9);
 
 const int ledPin = 4;
 const int servoPin = 5;
 int debug = HIGH;
 ServoTimer2 servo;
 
+EventQueue eventQueue;
+
 void setup()
 {
   Serial.begin(9600);
   bluetooth_serial.begin(9600);
 
-  pinMode(ledPin,OUTPUT);
-  pinMode(servoPin,OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(servoPin, OUTPUT);
 
   servo.attach(servoPin);
   servo.write(map(0, 0, 180, 750, 2250));
 
   pinMode(13, OUTPUT);
+
+  eventQueue = EventQueue();
 }
 
 void processSerial(Stream &serial, String source)
@@ -32,37 +37,41 @@ void processSerial(Stream &serial, String source)
     digitalWrite(13, debug);
     debug = !debug;
 
+    eventQueue.enqueue(Event(message));
+  } 
+}
+void handleEvent(Event &ev)
+{
+  Action action = MyMsg(ev.getCommand()).interpret();
 
-    Action action = MyMsg(message).interpret();
+  // Serial.print(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
 
-    //Serial.print(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
+  if (action.source.equals(SMARTPHONE))
+  {
+    Serial.print(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
+    // Serial.println(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
+  }
 
-    if (action.source.equals(SMARTPHONE))
-    {
-      Serial.print(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
-      // Serial.println(MyMsg("").createMessage(SMARTPHONE, BACKEND, action.ID, action.value));
-    }
+  switch (action.ID)
+  {
+  case LED_ID:
+    // Serial.println("led");
+    digitalWrite(ledPin, action.value > 0 ? HIGH : LOW);
+    break;
+  case SERVO_ID:
+    // Serial.println("servo");
+    servo.write(map(action.value, 0, 180, 750, 2250));
+    break;
+  default:
+    // Serial.println("default");
+    break;
+  }
 
-    switch (action.ID)
-    {
-    case LED_ID:
-      // Serial.println("led");
-      digitalWrite(ledPin, action.value > 0 ? HIGH : LOW);
-      break;
-    case SERVO_ID:
-      //Serial.println("servo");
-      servo.write(map(action.value, 0, 180, 750, 2250));
-      break;
-    default:
-      // Serial.println("default");
-      break;
-    }
-
-    if(source.equals("PC")){
-      String update = MyMsg("").createMessage(HARDUINO,SMARTPHONE,action.ID,action.value);
-      Serial.print("#"+update);
-      bluetooth_serial.print(update);
-    }
+  if (action.source.equals("PC"))
+  {
+    String update = MyMsg("").createMessage(HARDUINO, SMARTPHONE, action.ID, action.value);
+    Serial.print("#" + update);
+    bluetooth_serial.print(update);
   }
 }
 
@@ -71,8 +80,9 @@ void loop()
   processSerial(Serial, "PC");
   processSerial(bluetooth_serial, "BT");
 
-  //Serial.begin(9600);
-  //while(!Serial){}
-  //bluetooth_serial.begin(9600);
-  //while(!bluetooth_serial){}
+  while(!eventQueue.isEmpty()){
+    Event event = eventQueue.dequeue();
+    handleEvent(event);
+  }
 }
+
